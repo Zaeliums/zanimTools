a6369d9a18c201d84af419041471bf7a42380f47
Zaeliums <za.dussault@hotmail.com>
2025-01-30 09:13:38 -0500

    Reload module added to show Ui button

--- maya_button_ui.py
+++ maya_button_ui.py
@@ -1,2 +1,5 @@
+from zanimTools.module_reloader import reset_session_for_script
 from zanimTools.show_ui import show
+if __name__ == "__main__":
+    reset_session_for_script()
 show()
--- show_ui.py
+++ show_ui.py
@@ -1,15 +1,20 @@
-#import module_reloader
-from zanimTools.rig_setup.core.scene_data import NamingConvention
+from zanimTools.module_reloader import reset_session_for_script
+import os
+import sys
+
 import maya.cmds as cmds
+from zanimTools.rig_setup.core.scene_data import NamingConvention
 from zanimTools.rig_setup.ui.main_menu import MainMenu
 
-
-#if __name__ == "__main__":
-    #module_reloader.reset_session_for_script()    # (r"C:\Users\ZaDus\Documents\maya\scripts\zanimTools")
+# Add the scripts folder to the system path
+scripts_path = os.path.join(os.path.expanduser("~"), "documents", "maya", "scripts")
+if scripts_path not in sys.path:
+    sys.path.append(scripts_path)
 
 
 # Import the main menu function
 def show():
+
     if cmds.window("rigSetupUI", exists=True):
         cmds.deleteUI("rigSetupUI")
     naming_convention = NamingConvention()
